bfe24fa5aebec7ec3b791cff600b9a717f7d0dfc
James Wyatt <78618659+Arctic-Gif@users.noreply.github.com>
2022-11-14 17:31:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/happybirthdayapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.happybirthdayapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.happybirthdayapp.ui.theme.HappyBirthdayAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    BirthdayGreetingWithText("Happy Birthday James!", "From: Marlene")
+                }
+            }
+        }
+    }
+}
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(text = message, fontSize = 36.sp)
+        Text(text = from, fontSize = 24.sp)
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HappyBirthdayAppTheme {
+        BirthdayGreetingWithText("Happy Birthday James!", "From: Marlene")
+    }
+}
+
+
